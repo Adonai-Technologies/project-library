62db2fb0d0e994d9dc592dcfb5ddb594623f17c5
Heavenba7 <akuakkonadu8@gmail.com>
2023-06-22 14:09:20 +0100

    book constructer object created

---
+++ myScript.js
@@ -0,0 +1,9 @@
+function Book() {
+    this.title = "Hunter";
+    this.author = "Eunice";
+    this.pages = "345";
+    this.read = "Not read yet";
+    hunter.info() = function () {
+        return;
+    };
+}
