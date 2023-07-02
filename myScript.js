2517d8cab40e87b4d9cf22fcc71ca34be4c64a10
Heavenba7 <akuakkonadu8@gmail.com>
2023-07-02 13:38:35 +0100

    adding JavaScript

---
+++ myScript.js
@@ -0,0 +1,20 @@
+// Variables
+const titleInput = document.querySelector("#title");
+const authorInput = document.querySelector("#author");
+const isbnInput = document.querySelector("#isbn");
+const button = document.querySelector(".btn")
+const bookList = document.querySelector("book-list")
+
+button.addEventListener("click", function () {
+    if (
+        titleInput.value == "" &&
+        authorInput.value == "" &&
+        isbnInput.value == ""
+
+    ) {
+        alert("Enter any input")
+    } else {
+     const bookListRow = document.createElement("tr")
+    }
+
+})
